0c81ae2c973c0508752a32648a7b2b2778cbf6e0
robotzhang <445679586@qq.com>
2012-11-20 16:45:35 +0800

    本地运行版本1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nvyoudaquan::Application.config.secret_token = '0951c056649b9f91e7b03a986763d77180f199c1a6bbc59dbc97413d64a30e6d8e88152041352d2972bef2487cb2c3179f05885b50864b9600e34d20b359b454'

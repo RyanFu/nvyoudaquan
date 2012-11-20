33d0fc9c039393786f0f4ba73d0f9a2c62ec0bfa
robotzhang <445679586@qq.com>
2012-11-20 21:05:57 +0800

    helper方法放错了地方

--- app/helpers/girls_helper.rb
+++ app/helpers/girls_helper.rb
@@ -1,2 +1,23 @@
+#coding=utf-8
 module GirlsHelper
+  def title_girl(girl)
+    keywords = girl.name_cn
+    keywords += '_' + girl.name_cn + '电影'
+    keywords += '_' + girl.name_cn + '最新电影'
+    keywords += '_' + girl.name_cn + '图片'
+    keywords += '_' + girl.name_cn + '全集'
+    keywords += '_' + '(女优大全)'
+    keywords
+  end
+  def keywords_girl(girl)
+    keywords = girl.name_cn
+    keywords += '，' + girl.name_cn + '电影'
+    keywords += '，' + girl.name_cn + '的电影'
+    keywords += '，' + girl.name_cn + '全集'
+    keywords += '，' + girl.name_cn + '的全集'
+    keywords += '，' + girl.name_cn + '图片'
+    keywords += '，' + girl.name
+    keywords += '，' +  girl.name_en
+    return keywords
+  end
 end
